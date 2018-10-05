4a71b5e589048f4b404fe748c6785a31f9e96ccc
Inka L <inka.lappalainen@hotmail.com>
2018-10-06 00:01:51 +0300

    vko5 teht15, places_spec testit rikki

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,6 +9,7 @@ class PlacesController < ApplicationController
 
   def search
     @places = BeermappingApi.places_in(params[:city])
+    @weather = ApixuApi.weather_in(params[:city])
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
